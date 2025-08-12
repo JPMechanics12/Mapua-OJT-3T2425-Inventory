f82b99d48dba8e932b9763c8be5222bbe764c96c
JPMechanics12 <161663033+JPMechanics12@users.noreply.github.com>
2025-08-12 17:40:11 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ DB_CONFIG = {
     'host': '127.0.0.1', 
     'port': 3306,
     'user': 'root',
-    'password': 'James1203@', 
+    'password': 'admin12345', 
     'database': 'mapuainventory',
     'time_zone': '+08:00' # Set MySQL timezone to UTC+8 (Philippines)
 }
